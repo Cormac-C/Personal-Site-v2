431d378658b4445fb1296d977e1d527aa2816ae2
Cormac <cormaccureton@gmail.com>
2021-08-15 18:10:08 -0400

    Styling for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import { Navbar, NavbarBrand, Nav, Container, Button } from "react-bootstrap";
+import './navbar.css'
 
 export default class NavBar extends Component {
     render(){
         return(
             <div>
-                <Navbar collapseOnSelect bg="light" expand="sm" fixed="top">
+                <Navbar collapseOnSelect bg="white" expand="sm" fixed="top">
                     <Container>
-                        <NavbarBrand> Cormac </NavbarBrand>
+                        <NavbarBrand> CC </NavbarBrand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                             <Nav>
                                 <Nav.Link href="/about">About</Nav.Link>
                                 <Nav.Link href="/projects">Projects</Nav.Link>
-                                <Button href="/contact">
+                                <Button variant="nav" href="/contact">
                                     Contact
                                 </Button>
                             </Nav>
