88fb824e1b6b88517ec616d0b3bd645a2b46d363
Cormac <cormaccureton@gmail.com>
2021-08-15 16:35:45 -0400

    Add basic file structure

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: 96,
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+}
+
+const AboutPage = () => {
+    return (
+      <main style={pageStyles}>
+        <title>About | Cormac</title>
+        <h1 style={headingStyles}>
+          This is an about page!
+        </h1>
+      </main>
+    )
+  }
+  
+  export default AboutPage
---
+++ src/pages/projects.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: 96,
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+}
+
+const ProjectsPage = () => {
+    return (
+      <main style={pageStyles}>
+        <title>Projects | Cormac</title>
+        <h1 style={headingStyles}>
+          This is an projects page!
+        </h1>
+      </main>
+    )
+  }
+  
+  export default ProjectsPage
