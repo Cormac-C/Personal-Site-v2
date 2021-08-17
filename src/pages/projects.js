7286f877b1b9748128ad727b69ddf46c5653344a
Cormac <cormaccureton@gmail.com>
2021-08-17 01:57:16 -0400

    Create basic projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,23 +1,34 @@
 import * as React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import NavBar from "../components/navbar";
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
 
 const ProjectsPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Projects | Cormac</title>
       <NavBar />
-      <h1 style={headingStyles}>This is an projects page!</h1>
+      <Container className="body">
+        <Row className="sectionTitle">
+          <h1> Projects </h1>
+        </Row>
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="../images/face.jpg" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </main>
   );
 };
