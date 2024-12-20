af696c86bf38ccbc4e3bfd3c5fe81d961bf6a4a8
Cormac Cureton <cormaccureton@gmail.com>
2024-12-19 20:12:16 -0500

    Add meta description

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,10 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "en" });
+  setHeadComponents([
+    <meta
+      name="description"
+      content="Cormac Cureton's Portfolio Website"
+      key="meta-description"
+    />
+  ]);
 };
