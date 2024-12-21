0533ac70bac45636ae2d0645d5edc22f6ccb26a8
Cormac Cureton <cormaccureton@gmail.com>
2024-12-21 16:00:17 -0500

    Switch icon colour based on theme

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,14 @@ export const isProjectOutdated = (endDate) => {
   // Project is out of date if the end date is more than 6 months ago
   return dateDiff > 1000 * 60 * 60 * 24 * 30 * 6;
 };
+
+export const getColorSchemePreference = () => {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return "dark";
+  } else {
+    return "light";
+  }
+};
