26c48cf5582e9462c0e163a262507deb2f3a0175
Cormac Cureton <cormaccureton@gmail.com>
2024-12-23 17:24:18 -0500

    Config ts

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-4 text-center">
+      <Container>
+        <Row className="justify-center">
+          <p className="text-xs mb-2">Built by Cormac Cureton 2024</p>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
