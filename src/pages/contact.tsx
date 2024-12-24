dc35bcc63b2bea89a4d2ed30d724ddbf8e88f519
Cormac Cureton <cormaccureton@gmail.com>
2024-12-24 17:29:06 -0500

    Update contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,6 +4,7 @@ import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import { Seo } from "../components/seo";
 import { StaticImage } from "gatsby-plugin-image";
+import ContactButton from "../components/copyButton";
 
 const ContactPage: React.FC = () => {
   return (
@@ -22,55 +23,46 @@ const ContactPage: React.FC = () => {
         </Row>
         <Row>
           <Col className="justify-center col-4" align="center">
-            <a
-              href="mailto: cormaccureton@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <StaticImage
-                src="../images/email.svg"
-                alt="Email Icon"
-                placeholder="dominantColor"
-                loading="eager"
-                layout="fixed"
-                width={100}
-                height={100}
-              />
-            </a>
+            <StaticImage
+              src="../images/email.svg"
+              alt="Email Icon"
+              placeholder="dominantColor"
+              loading="eager"
+              layout="fixed"
+              width={100}
+              height={100}
+            />
+            <ContactButton
+              text="cormaccureton@gmail.com"
+              email="cormaccureton@gmail.com"
+            />
           </Col>
           <Col className="justify-center col-4" align="center">
-            <a
-              href="https://github.com/Cormac-C"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <StaticImage
-                src="../images/github.svg"
-                alt="Github Icon"
-                placeholder="dominantColor"
-                loading="eager"
-                layout="fixed"
-                width={100}
-                height={100}
-              />
-            </a>
+            <StaticImage
+              src="../images/github.svg"
+              alt="Github Icon"
+              placeholder="dominantColor"
+              loading="eager"
+              layout="fixed"
+              width={100}
+              height={100}
+            />
+            <ContactButton text="Cormac-C" link="https://github.com/Cormac-C" />
           </Col>
           <Col className="justify-center col-4" align="center">
-            <a
-              href="https://www.linkedin.com/in/cormaccureton"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <StaticImage
-                src="../images/linkedin.svg"
-                alt="Linkedin Icon"
-                placeholder="dominantColor"
-                loading="eager"
-                layout="fixed"
-                width={100}
-                height={100}
-              />
-            </a>
+            <StaticImage
+              src="../images/linkedin.svg"
+              alt="Linkedin Icon"
+              placeholder="dominantColor"
+              loading="eager"
+              layout="fixed"
+              width={100}
+              height={100}
+            />
+            <ContactButton
+              text="cormaccureton"
+              link="https://www.linkedin.com/in/cormaccureton/"
+            />
           </Col>
         </Row>
       </Container>
