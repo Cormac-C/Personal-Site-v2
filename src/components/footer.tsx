b3eb2e8f813f60004fa0da15cb26f8db3375dea1
Cormac Cureton <cormaccureton@gmail.com>
2025-01-02 12:22:06 -0500

    Update footer again

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <div className="w-full h-4 text-center">
       <Container>
         <Row className="justify-center">
-          <p className="text-xs mb-2">Built by Cormac (Me) 2025</p>
+          <p className="text-xs mb-2">Built by Me (Cormac) 2025</p>
         </Row>
       </Container>
     </div>
