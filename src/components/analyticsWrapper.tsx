9fa968543e952165fde577ce794419bd62ecc4b1
Cormac Cureton <cormaccureton@gmail.com>
2025-01-04 11:42:47 -0500

    Add speed insights to wrapper

--- src/components/analyticsWrapper.tsx
+++ src/components/analyticsWrapper.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const AnalyticsWrapper = ({ children }: any) => {
   return (
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 };
