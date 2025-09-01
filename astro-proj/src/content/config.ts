aca186c6e150dfe2e2ba0c59695a611661d9e536
Cormac Cureton <cormaccureton@gmail.com>
2025-09-01 15:12:45 -0400

    Setup project pages

--- astro-proj/src/content/config.ts
+++ astro-proj/src/content/config.ts
@@ -1,15 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const projects = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     tech: z.string(),
     blurb: z.string(),
     hero_image: z.string().optional(),
     hero_image_alt: z.string().optional(),
+    demo_gif: z.string().optional(),
     endDate: z.string(),
-    hide: z.string().transform(v => v === 'true' ? 'true' : 'false')
+    hide: z.string().transform((v) => (v === "true" ? "true" : "false"))
   })
 });
 
