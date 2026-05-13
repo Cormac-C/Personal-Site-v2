bd103ba7ecfd5819a40caf96f6f15580bf4af3e2
Cormac Cureton <cormaccureton@gmail.com>
2026-05-13 11:51:02 -0400

    Adjust CSS handling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,15 @@
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   theme: {
     extend: {
       colors: {
-        'theme-blue': '#597ede',
-        'theme-blue-accent': '#4160b0',
-        'theme-blue-light': '#cdd8f5'
+        "theme-blue": "#597ede",
+        "theme-blue-accent": "#4160b0",
+        "theme-blue-light": "#cdd8f5"
       },
       spacing: {
-        'body-min': 'calc(100vh - 128px - 1rem)',
-        'max-margin': 'max(128px, 10%)'
+        "body-min": "calc(100vh - 128px - 1rem)",
+        "max-margin": "max(128px, 10%)"
       }
     }
   },
